e641dd4008179176f6e69918b32e371cdab4d5aa
yuxin <zyx954@gmail.com>
2017-07-11 16:09:19 +1000

    update ReadMe

--- DataCollectionScript.py
+++ DataCollectionScript.py
@@ -20,10 +20,10 @@ def oauth_login():
     # on Twitter's OAuth implementation.
 
     # use your own key
-    CONSUMER_KEY = ''
-    CONSUMER_SECRET = ''
-    OAUTH_TOKEN = ''
-    OAUTH_TOKEN_SECRET = ''
+    CONSUMER_KEY = 'XXX'
+    CONSUMER_SECRET = 'XXX'
+    OAUTH_TOKEN = 'XXX'
+    OAUTH_TOKEN_SECRET = 'XXX'
 
     auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                                CONSUMER_KEY, CONSUMER_SECRET)
@@ -35,29 +35,10 @@ def oauth_login():
 # Sample usage
 twitter_api = oauth_login()
 
-# everytime this script will collect 7 files & when it's over, I just need to
-#  uncommit another fileNames -->split into 7 for checking purpose which
-# means I need to do a update every 7-8 days
-fileNames = ['xbf', 'xbg', 'xbh', 'xbi', 'xbj']
-# fileNames = ['xbk','xbl','xbm','xbn','xbo','xbp','xbq']
-# fileNames = ['xbr','xbs','xbt','xbu','xbv','xbw','xbx']
-# fileNames = ['xby','xbz','xca','xcb','xcc','xcd','xce']
-# fileNames = ['xcf','xcg','xch','xci','xcj','xck','xcl']
-# fileNames = ['xcm','xcn','xco','xcp','xcq','xcr','xcs']
-# fileNames = ['xct','xcu','xcv','xcw','xcx','xcy','xcz']
-# fileNames = ['xda','xdb','xdc','xdd','xde','xdf','xdg']
-# fileNames = ['xdh','xdi','xdj','xdk','xdl','xdm','xdn']
-# fileNames = ['xdo','xdp','xdq','xdr','xds','xdt','xdu']
-# fileNames = ['xdv','xdw','xdx','xdy','xdz','xea','xeb']
-# fileNames = ['xec','xed','xee','xef','xeg','xeh','xei']
-# fileNames = ['xej','xek','xel','xem','xen','xeo','xep']
-# fileNames = ['xeq','xer','xes','xet','xeu','xev','xew']
-# fileNames = ['xex','xey','xez','xfa','xfb','xfc','xfd']
-# fileNames = ['xfe','xff','xfg','xfh','xfi','xfj','xfk']
-# fileNames = ['xfl','xfm','xfn','xfo','xfp','xfq','xfr']
-# fileNames = ['xfs','xft','xfu','xfv','xfw','xfx','xfy']
+
+fileNames = ['xaa', 'xab', 'xac']
 for filename in fileNames:
-    # the filename formate is  'xbe'  'xbf'
+    # the filename formate is  'xba'  'xbb'
     #======get data from file to ListDict
 
     #manipulate  filename into  the formate as "./SplitAs90000/xbe"
@@ -105,17 +86,6 @@ for filename in fileNames:
             tweetsResonse.maliciousResult = DataDict["maliciousResult"]
             tweetsResonse.annotationMethod = DataDict["annotationMethod"]
             List4TweetsResponse.append(tweetsResonse)
-        # except tweepy.RateLimitError:
-        #     time.sleep(60 * 15)
-        #     # here sleep 15mins and drop the previouse id. No matter the id is valide or not. kind of bug
-        #     print "rete limited , wait 15mins "
-        #     continue
-
-            # print json.dumps(List4TweetsResponse[0], indent=1)
-            # print d["tweetsID"]
-            # g=9+3
-            # r=g/0
-            # print List4TweetsResponse[0]
         except:
             print "catch error for " + DataDict["tweetsID"]
             errorCounter= errorCounter+1
@@ -142,15 +112,3 @@ for filename in fileNames:
         output.close()
 
 #==============finish writing data to file======
-
-
-
-#ListItems is list contains dic which can is the three values in dataset
-
-# print ListItems[0]["tweetsID"]
-# print twitter_api.statuses.show(_id=ListItems[0]["tweetsID"])
-
-
-#
-# s = twitter_api.statuses.show(_id=838727227256745984)
-# print s
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# TweetsCollecton
-A simple Script to collect tweets 
+# Introduction 
+
+A simple script to collect Tweets Based on Tweets ID. The samle IDs are in SplitAs9000Folder.
+
+## Getting Started
+
+This Script uses [Twitter][TwitterUrl] Module as API.
+[TwitterUrl]: http://mike.verdone.ca/twitter/#install 
+
+### Installing
+
+Twitter Module
+
+```
+Pip install twitter 
+```
+
+## Running the tests
+
+Enter your own Tweets Keys. Tweets will collected based the IDs in Folder SplitAs90000.
+
